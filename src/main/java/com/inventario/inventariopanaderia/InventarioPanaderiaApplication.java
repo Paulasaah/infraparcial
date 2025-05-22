7075fd53919a548c666429818300e9e0a65a628b
Paulasaah <paulasaavedra1101@gmail.com>
2025-05-21 19:28:11 -0500

    primer commit/Inicializacion del proyecto

---
+++ src/main/java/com/inventario/inventariopanaderia/InventarioPanaderiaApplication.java
@@ -0,0 +1,13 @@
+package com.inventario.inventariopanaderia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioPanaderiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioPanaderiaApplication.class, args);
+	}
+
+}
